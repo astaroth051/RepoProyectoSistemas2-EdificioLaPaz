65cb5a0f3a62f844ca38420c4346337b0bdee654
victor <astarth83@gmail.com>
2025-04-17 11:25:51 -0400

    cambiosConfiguracion

---
+++ EdificioLaPaz/resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Bienvenido a mi aplicación Vue.js</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #333;
+}
+</style>
